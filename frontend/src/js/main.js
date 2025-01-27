257b2047a9dbd47982de25a7827321581ac95872
bwolf <vagrant@bwolf>
2025-01-27 17:16:50 +0000

    SURPRISE BUTTON test font style theme etc

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,14 +1,34 @@
 console.log("main script started");
 
+let currentThemeIndex = 0;
+const themes = ["theme1", "theme2", "theme3", "theme4"];
+
+function setNewTheme() {
+  document.body.classList.remove(...themes);
+
+  document.body.classList.add(themes[currentThemeIndex]);
+  
+  currentThemeIndex++;
+
+  // Si on dépasse le dernier index, on revient à 0
+  if (currentThemeIndex >= themes.length) {
+    currentThemeIndex = 0;
+  }
+}
+function loadLoginPage() {
+  const app = document.getElementById("app");
+  app.innerHTML = `
+    <div class="container d-flex flex-column justify-content-center align-items-center" style="height:100vh;">
+      <button class="btn connect-btn mb-3" id="connectButton">CONNECT WITH 42</button>
+      <button class="btn btn-warning" id="themeButton">Surprise</button>
+    </div>
+  `;
+  document.getElementById("themeButton").addEventListener("click", () => {
+    setNewTheme();
+  });
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     loadLoginPage();
   });
-  
-  function loadLoginPage() {
-    const app = document.getElementById("app");
-    app.innerHTML = `
-      <div class="container d-flex justify-content-center align-items-center" style="height:100vh;">
-        <button class="btn connect-btn" id="connectButton">CONNECT WITH 42</button>
-      </div>
-    `;
-  }
+
