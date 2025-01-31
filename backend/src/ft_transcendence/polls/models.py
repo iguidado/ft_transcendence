12f9b3ad526bb6ed0439953810143181db16f84c
Bugs <bugs@fedora>
2025-01-31 13:06:14 +0100

    fin tuto django

--- backend/src/ft_transcendence/polls/models.py
+++ backend/src/ft_transcendence/polls/models.py
@@ -10,6 +10,9 @@ class Question (models.Model):
 	def __str__(self):
 		return self.question_text
 
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
@@ -17,7 +20,5 @@ class Choice(models.Model):
 
 	def __str__(self):
 		return self.choice_text
-	
-	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
