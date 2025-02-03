e618fc9d527962fc04891ebba9e2acd237918b22
Bugs <bugs@fedora>
2025-02-03 14:01:16 +0100

    basic api implementation

--- backend/setup.sh
+++ backend/setup.sh
@@ -16,5 +16,17 @@ then
 	mkdir -p /app/ft_transcendence
 	django-admin startproject billpong /app/ft_transcendence
 fi
+	
+if ! [ -d /app/ft_transcendence/api ]
+then
+	cd /app/ft_transcendence
+	django-admin startapp api
+fi
 
 python /app/ft_transcendence/manage.py runserver 0.0.0.0:8000
+
+python /app/ft_transcendence/manage.py migrate
+
+python /app/ft_transcendenc/manage.py makemigrations api
+
+# python manage.py createsuperuser
