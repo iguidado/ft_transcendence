32f2d1322a5bd615dc7454a67c8b2ff102f1e7b7
iguidado <noob.3vol@gmail.com>
2025-02-26 13:51:16 +0100

    Feat: Added ELK security through SSL/TLS

--- ELK/Elasticsearch/entrypoint.sh
+++ ELK/Elasticsearch/entrypoint.sh
@@ -11,34 +11,33 @@ fi;
 
 
 ## Creating certs
-if [ ! -d config/certs ]; then
-	if [ ! -f config/certs/ca.zip ]; then
-		echo "Creating CA";
-		bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
-		unzip config/certs/ca.zip -d config/certs;
-	fi;
+if [ ! -f config/certs/ca.zip ]; then
+	echo "Creating CA";
+	bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
+	unzip config/certs/ca.zip -d config/certs;
+fi;
 
 
-	if [ ! -f config/certs/certs.zip ]; then
-		echo "Creating certs";
-		echo -ne \
-			"instances:\n"\
-			"  - name: es01\n"\
-			"    dns:\n"\
-			"      - es01\n"\
-			"      - localhost\n"\
-			"    ip:\n"\
-			"      - 127.0.0.1\n"\
-			"  - name: kibana\n"\
-			"    dns:\n"\
-			"      - kibana\n"\
-			"      - localhost\n"\
-			"    ip:\n"\
-			"      - 127.0.0.1\n"\
-			> config/certs/instances.yml;
-					bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
-					unzip config/certs/certs.zip -d config/certs;
-	fi;
+if [ ! -f config/certs/certs.zip ]; then
+	echo "Creating certs";
+	echo -ne \
+		"instances:\n"\
+		"  - name: es01\n"\
+		"    dns:\n"\
+		"      - es01\n"\
+		"      - localhost\n"\
+		"    ip:\n"\
+		"      - 127.0.0.1\n"\
+		"  - name: kibana\n"\
+		"    dns:\n"\
+		"      - kibana\n"\
+		"      - localhost\n"\
+		"    ip:\n"\
+		"      - 127.0.0.1\n"\
+		> config/certs/instances.yml;
+			bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
+			unzip config/certs/certs.zip -d config/certs;
+fi;
 
 echo "Setting file permissions"
 chown -R root:root config/certs;
@@ -58,17 +57,6 @@ until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASS
 do
 	sleep 10;
 done;
-elif
-
-echo "Setting kibana_system password";
-#------------------------------------------------
-until curl -s -X POST -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}";
-do
-	sleep 10;
-done;
-
-fi
-
 
 
 echo "All done!";
