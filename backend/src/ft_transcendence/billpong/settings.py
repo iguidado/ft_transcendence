5b5fea47bf6cfe8fe9ae90cfd4a56fec74e434dd
Leon PERROTIN <lperroti@bess-f2r5s12.clusters.42paris.fr>
2025-03-22 18:45:51 +0100

    DEBUG CORS

--- backend/src/ft_transcendence/billpong/settings.py
+++ backend/src/ft_transcendence/billpong/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-9az0mj7ne4t(n4by9l4-!2cv-dimmzlbr%32-2@@(#ljb3sfj^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -98,6 +98,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -105,11 +106,34 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-	'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8888",
+    "http://localhost:4000",
+    "http://127.0.0.1:4000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+# Add these settings after your existing CORS configuration
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 ]
 
 ROOT_URLCONF = 'billpong.urls'
