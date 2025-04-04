1e65633d4021ba43bc7adcdcee232998fe2a8c41
Leon PERROTIN <lperroti@bess-f2r6s6.clusters.42paris.fr>
2025-04-04 03:29:45 +0200

    tabs or spaces we need to choose

--- frontend/src/js/build.js
+++ frontend/src/js/build.js
@@ -1,32 +1,40 @@
 import { load_page } from "./router";
+import { getAccessToken } from "./utils/getAccessToken";
 
-function toggleLayerPair(event){
-    // console.log('toggleLayerPair:', event);
-    const btn = event.currentTarget;
-    const onId = btn.getAttribute('data-on');
-    const offId = btn.getAttribute('data-off');
-    const onEl = document.getElementById(onId);
-    const offEl = document.getElementById(offId);
+function toggleLayerPair(event) {
+	// console.log('toggleLayerPair:', event);
+	const btn = event.currentTarget;
+	const onId = btn.getAttribute("data-on");
+	const offId = btn.getAttribute("data-off");
+	const onEl = document.getElementById(onId);
+	const offEl = document.getElementById(offId);
 
-    if (offEl.style.display !== 'none') {
-        offEl.style.display = 'none';
-        onEl.style.display = 'block';
-    } else {
-        offEl.style.display = 'block';
-        onEl.style.display = 'none';
-    }
+	if (offEl.style.display !== "none") {
+		offEl.style.display = "none";
+		onEl.style.display = "block";
+	} else {
+		offEl.style.display = "block";
+		onEl.style.display = "none";
+	}
 }
 
-export function initBuildButtons(){
-    const hotspots = document.querySelectorAll('[data-off][data-on]');
-    hotspots.forEach(btn => {
-      btn.addEventListener("mouseover", toggleLayerPair);
-      btn.addEventListener("mouseout", toggleLayerPair);
-      const loaddest = btn.getAttribute('data-load');
-      if (loaddest) {
-        btn.addEventListener("click", () => {
-          load_page(loaddest);
-        });
-      }
-    });
+export function initBuildButtons() {
+	// TODO Conditional display buttons
+	// if (getAccessToken()) {
+	// 	console.log("connected !")
+	// 	// not display back to login
+	// } else {
+	// 	console.log("disconnected !")
+	// }
+	const hotspots = document.querySelectorAll("[data-off][data-on]");
+	hotspots.forEach((btn) => {
+		btn.addEventListener("mouseover", toggleLayerPair);
+		btn.addEventListener("mouseout", toggleLayerPair);
+		const loaddest = btn.getAttribute("data-load");
+		if (loaddest) {
+			btn.addEventListener("click", () => {
+				load_page(loaddest);
+			});
+		}
+	});
 }
