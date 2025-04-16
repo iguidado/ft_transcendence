3b7bd8fe53df96fb345a9aeb0d00fb78b15ab5a1
devinedev <devinedev@pong.lol>
2025-04-16 15:17:11 +0200

    wip

--- frontend/src/js/game_pages/tournamentSetupPage/loadTournamentSetupPage.js
+++ frontend/src/js/game_pages/tournamentSetupPage/loadTournamentSetupPage.js
@@ -4,4 +4,18 @@ export async function loadTournamentSetupPage(ctx) {
 	const app = document.getElementById("main_container");
 	const torunamentHtml = await fetchHTMLContent("tournament")
 	app.innerHTML = torunamentHtml
+	displayPlayerList()
+	setupAddPlayerBtn()
+}
+
+function displayPlayerList() {
+	const container = document.getElementById("")
+}
+
+function setupAddPlayerBtn() {
+	const btn = document.getElementById("tournament__startbtn")
+	btn.onclick = e => {
+		e.preventDefault()
+		console.log("TEST")
+	}
 }
