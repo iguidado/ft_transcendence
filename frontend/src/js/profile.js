a680ee66b7a715d35748d550a47b97747efa2134
barbiloup <barbarawolf99@gmail.com>
2025-04-17 11:35:43 +0200

    wip

--- frontend/src/js/profile.js
+++ frontend/src/js/profile.js
@@ -96,6 +96,7 @@ function toggle2faError(err, res) {
 	console.warn("API Response:", res)
 }
 
+//TODO modale settings pour tournament
 //save settings DONE
 function saveSettings() {
 
@@ -108,6 +109,7 @@ function saveSettings() {
 			console.log("Nom d'utilisateur mis à jour avec succès :", response);
 			document.getElementById("usernameDisplay").textContent =
 			  newDisplayName.charAt(0).toUpperCase() + newDisplayName.slice(1);
+			  pullProfile();
 		  }, (error) => {
 			console.error("Erreur lors de la mise à jour du nom d'utilisateur :", error);
 		  });
